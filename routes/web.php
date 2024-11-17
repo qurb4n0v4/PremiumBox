aaf7e563c3acbf163b87beee03405b60f1fb6f74
GulnurMirzayeva <gmirzyeva1@gmail.ru>
2024-11-17 19:51:45 +0400

    About us controllers and routes

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\ContactUsController;
+use App\Http\Controllers\FaqController;
+use App\Http\Controllers\PrivacyPolicyController;
 
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about-us', [AboutUsController::class, 'index'])->name('about-us');
+Route::get('/contact-us', [ContactUsController::class, 'index'])->name('contact-us');
+Route::get('/faq', [FaqController::class, 'index'])->name('faq');
+Route::get('/privacy-policy', [PrivacyPolicyController::class, 'index'])->name('privacy-policy');
+
 
