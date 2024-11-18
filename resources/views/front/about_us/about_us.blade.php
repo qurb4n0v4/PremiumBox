43ba2447c937345440a70232f1a9eb691795e7d9
ilkin3333 <ihumbetov09@gmail.com>
2024-11-18 23:47:34 +0400

    about_us created

--- resources/views/front/about_us/about_us.blade.php
+++ resources/views/front/about_us/about_us.blade.php
@@ -1,1 +1,82 @@
-<h1>hi</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>about-us</title>
+    <link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+<div class="about-main">
+    <div class="about-box">
+        <div class="about-left">
+            <h1>Box & Tale</h1>
+            <h3>Ilkin Humbatov tərəfindən</h3>
+            <img src="./images/979.webp" alt="" />
+        </div>
+        <div class="about-right">
+            <p>
+                Box and Tale onlayn əsaslı hədiyyə təchizatçısıdır. Biz burada,
+                müştərilərimizə hədiyyə qutası və içindəkiləri hazırlamaqda kömək
+                edirik, beləliklə, özəl gününüzü sevdiklərinizlə tamamlaya
+                bilərsiniz. Hədiyyə qutası içeriğini seçmək üçün məhsul çeşidimizdən
+                seçim edə bilərsiniz.
+                <br /><br />
+                Biz inanırıq ki, hər bir müştərimizin yaxın insanları ilə xüsusi və
+                bənzərsiz əlaqələri var, buna görə də biz müştərilərimizə öz şəxsi
+                toxunuşlarını hədiyyələr vasitəsilə ifadə etmək imkanı təqdim
+                edirik. Bu hədiyyə qutuları hər bir alıcıya uyğun olaraq xüsusi
+                hazırlanır və müştərilərimizin unikal ehtiyaclarını təmsil edir.
+                <br /><br />
+                Eyni zamanda, müştərilərimiz üçün hədiyyə alış-verişini daha
+                əlverişli etmək məqsədini güdürük. Biz istifadəçi dostu veb sayt
+                sistemi ilə hədiyyə almağı asanlaşdırırıq, beləliklə, sadəcə bir
+                neçə kliklə saytımızda seçimlərinizi edirsiniz və qalanını biz təmin
+                edirik!
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="howworks">
+    <div class="howmain">
+        <div class="howworkswrite">
+            <h1>Necə işləyir?</h1>
+        </div>
+        <div class="worksetap">
+            <div class="etapsnumber">
+                <p>Qutu qurun və ya əvvəlcədən hazırlanmış qutularımızı seçin</p>
+            </div>
+            <div class="etapsnumber">
+                <p>Sifariş verin və ödənişi tamamlayın</p>
+            </div>
+            <div class="etapsnumber">
+                <p>2-5 gün istehsal</p>
+            </div>
+            <div class="etapsnumber">
+                <p>Hədiyyə Seçdiyiniz Kuryerlə Çatdırılır</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="webegin">
+    <div class="webeginmain">
+        <div class="beginleft">
+            <h2>Başlayaq?</h2>
+            <p class="beginnormalp">Öz Fərdi Hədiyyənizi Yaradın</p>
+            <button>QUTU QUR</button>
+            <p class="beginnormalp">Hazır Hədiyyə Paketləri alın</p>
+            <button>HAZIRLANMIŞ QUTU</button>
+            <p class="beginnormalp">Korporativ Hədiyyələr və Xüsusi Layihələr</p>
+            <button>KORPORATİV HƏDİYYƏLƏR</button>
+            <p class="formoreinfo">Əlavə məlumat üçün Bizimlə əlaqə saxlayın</p>
+        </div>
+        <div class="beginright">
+            <img src="./images/980.webp" alt="" />
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
