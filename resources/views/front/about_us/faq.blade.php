37e55475a9db63dc5ed8f9b08c4cb872d98cd212
ilkin3333 <ihumbetov09@gmail.com>
2024-11-19 13:46:58 +0400

    faq, contact, privacy created

--- resources/views/front/about_us/faq.blade.php
+++ resources/views/front/about_us/faq.blade.php
@@ -4,5 +4,148 @@
 
 @section('content')
 
+<div class="faq-main">
+    <div class="faq-box">
+        <div class="faq-right">
+        <div class="faq-section">
+        <h1>Tez-tez verilən suallar</h1>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Box & Tale hədiyyə qutularının ölçüləri nə qədərdir?</div>
+            <div class="faq-answer">C: Standart qutu (21x25x10sm) və Böyük qutu (25x30x12sm).</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: "Qutu Yarat" və "Hazır Qutu" arasında fərq nədir?</div>
+            <div class="faq-answer">C: "Qutu Yarat" müştərinin qutunu, hədiyyələri və kartı öz zövqünə görə yaratmasına imkan verir. "Hazır Qutu" isə nə hədiyyə alacağınıza qərar verə bilmədikdə və ya ideyaya ehtiyacınız olduqda istifadə edilə bilər.</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Hədiyyənin qiyməti nə qədərdir?</div>
+            <div class="faq-answer">C: Qiymət qutuya əlavə etdiyiniz əşyalara və ya seçdiyiniz hazır paketin növünə görə dəyişir, buna görə büdcənizə uyğunlaşdıra bilərsiniz.</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Bir qutuya neçə əşya yerləşə bilər?</div>
+            <div class="faq-answer">C: Qutunun məzmununu müştəri müəyyən edir, buna görə də əşyaların sayı dəyişir. Qutunun dolduğu barədə bildiriş alacaqsınız.</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Sadəcə qutunu sifariş edə bilərəmmi və onun daxilində nələr var?</div>
+            <div class="faq-answer">C: Əlbəttə, yalnız qutunu sifariş edə bilərsiniz. Onun daxilində hədiyyə kartı, lent, kağız parçaları və salfet kağızı var.</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Qutudan başqa yalnız hədiyyə əşyalarını sifariş edə bilərəmmi?</div>
+            <div class="faq-answer">C: Hər bir sifarişə qutu daxil edilməlidir.</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Qutunun hazırlanması nə qədər vaxt alır?</div>
+            <div class="faq-answer">C: Sifarişdən çatdırılmaya qədər bütün proses 2-7 gün çəkir. Personalizasiya səviyyəsinə görə bu müddət uzana bilər.</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Qutunun çatdırılma vaxtını necə hesablaya bilərəm?</div>
+            <div class="faq-answer">C: Müştərilər çatdırılma vaxtını sifariş formasında təyin edə bilərlər. Box & Tale sifarişi göstərilən tarixdə göndərəcək (xahiş edirik, gözlənilməz gecikmələr üçün əlavə vaxt nəzərə alın).</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Box & Tale hansı çatdırılma xidmətlərini təklif edir?</div>
+            <div class="faq-answer">C: Box & Tale JNE (bütün İndoneziya ərazisini əhatə edir) və GoSend (Jabodetabek ərazisi üçün) kimi kuryer xidmətlərindən istifadə edir.</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Hədiyyəni digər şəhərlərə çatdırmaq mümkündürmü?</div>
+            <div class="faq-answer">C: Box & Tale İndoneziya daxilində bütün ərazilərə çatdırılma edir.</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Beynəlxalq çatdırılma mövcuddurmu?</div>
+            <div class="faq-answer">C: Hal-hazırda Box & Tale yalnız İndoneziya daxilində çatdırılma edir. Lakin, xaricdə yaşayan müştərilər üçün dəstək göstərə bilərik.</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Hədiyyəni birbaşa alıcıya göndərmək mümkündürmü?</div>
+            <div class="faq-answer">C: Əlbəttə! Çatdırılma ünvanına alıcının məlumatlarını daxil edin və biz onu birbaşa onlara göndərərik!</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Xüsusi tələblərim olarsa nə etməliyəm?</div>
+            <div class="faq-answer">C: Çatdırılma tarixi və ya qutu məzmunu ilə bağlı xüsusi tələblərinizi sifariş zamanı qeyd sahəsinə yaza bilərsiniz və ya bizimlə WhatsApp və Line vasitəsilə əlaqə saxlayın.</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Hansı ödəniş üsulları mövcuddur?</div>
+            <div class="faq-answer">C: Kredit kartı, bank köçürməsi, Gopay, Ovo, Shopeepay və s. ödəniş üsullarını qəbul edirik. Xarici ödənişlər üçün PayPal da mövcuddur.</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Qutu çatdırılma zamanı zədələnsə nə etməliyəm?</div>
+            <div class="faq-answer">C: Göndərilmədən əvvəl qutunun şəklini sizə təqdim edirik. Zərər olarsa, məhsulları şəkilə əsasən yenidən düzəldə bilərsiniz.</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Sifarişlə bağlı problem olarsa nə etməliyəm?</div>
+            <div class="faq-answer">C: Problem yaranarsa, dərhal bizimlə əlaqə saxlayın.</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Korporativ hədiyyələr üçün xidmət təklif olunurmu?</div>
+            <div class="faq-answer">C: Bəli, Box & Tale böyük partiyalar üçün korporativ hədiyyələr və xüsusi layihələr təklif edir.</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Korporativ hədiyyələr üçün haradan sifariş edə bilərəm?</div>
+            <div class="faq-answer">C: WhatsApp (0818 1818 4158) vasitəsilə bizimlə əlaqə saxlayın, komandamız sizə kömək edəcək.</div>
+        </div>
+
+        <div class="faq-item">
+            <div class="faq-question">S: Digər suallarım olarsa nə etməliyəm?</div>
+            <div class="faq-answer">C: WhatsApp (081311033691) və Line (@boxandtale) vasitəsilə bizimlə əlaqə saxlayın.</div>
+        </div>
+    </div>
+        </div>
+
+    </div>
+</div>
+
+<style>
+        .faq-main{
+        width: 100%;
+        height: auto;
+        background-color: rgb(248,248,248);
+        display: flex;
+        justify-content: center;
+    }
+    .faq-box {
+        margin-top: 24px;
+        margin-bottom: 24px;
+        width: 1100px;
+        border-radius: 10px;
+        height: auto;
+        background-color: white;
+        display: flex;
+        justify-content: space-between;
+        padding: 40px 25px;
+
+    }
+    .faq-section {
+            max-width: 800px;
+            margin: auto;
+        }
+        .faq-item {
+            margin-bottom: 20px;
+        }
+        .faq-question {
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+        .faq-answer {
+            margin-left: 20px;
+        }
+
+
+</style>
 
 @endsection
--- resources/views/front/about_us/privacy_policy.blade.php
+++ resources/views/front/about_us/privacy_policy.blade.php
@@ -4,5 +4,77 @@
 
 @section('content')
 
+<div class="privacy-main">
+    <div class="privacy-box">
+        <div class="privacy-right">
+        <h1>Məxfilik Siyasəti</h1>
+    <p><strong>Effektiv tarix:</strong> 12 Avqust 2020</p>
 
+    <h2>Giriş</h2>
+    <p>Box & Tale-ə xoş gəlmisiniz.</p>
+    <p>Box & Tale (“biz”, “bizi” və ya “bizim”) www.boxandtale.com veb saytını (“Xidmət”) idarə edir.</p>
+    <p>Məxfilik Siyasətimiz www.boxandtale.com ziyarətlərinizi tənzimləyir və Xidmətimizdən istifadə nəticəsində məlumatları necə topladığımızı, qoruduğumuzu və açıqladığımızı izah edir.</p>
+    <p>Məlumatlarınızı Xidmət təqdim etmək və onu təkmilləşdirmək üçün istifadə edirik. Xidmətdən istifadə etməklə bu siyasətə uyğun olaraq məlumatların toplanmasına və istifadəsinə razılıq vermiş olursunuz. Bu Məxfilik Siyasətində başqa cür göstərilmədikcə, burada istifadə olunan terminlər bizim Şərtlər və Qaydalarımızda təyin olunan mənaları daşıyır.</p>
+    <p>Şərtlər və Qaydalarımız (“Şərtlər”) Xidmətimizin bütün istifadəsini tənzimləyir və Məxfilik Siyasəti ilə birlikdə sizinlə bizim aramızda razılaşmanı (“razılaşma”) təşkil edir.</p>
+
+    <h2>Təriflər</h2>
+    <p><strong>XİDMƏT:</strong> Box & Tale tərəfindən idarə olunan www.boxandtale.com veb saytını nəzərdə tutur.</p>
+    <p><strong>ŞƏXSİ MƏLUMATLAR:</strong> fərdi müəyyən edən məlumatlardır (bizim sahib olduğumuz və ya əldə edə biləcəyimiz digər məlumatlarla birlikdə fərdi müəyyən etmək üçün istifadə edilə bilən məlumatlar).</p>
+    <p><strong>İSTİFADƏ MƏLUMATLARI:</strong> avtomatik olaraq toplanan məlumatlardır (məsələn, səhifədə qalma müddəti kimi).</p>
+    <p><strong>COOKİE:</strong> cihazınızda (kompüter və ya mobil cihazda) saxlanılan kiçik fayllardır.</p>
+    <p><strong>MƏLUMAT NƏZARƏTÇİSİ:</strong> şəxsi məlumatların işlənməsi məqsədini və üsulunu müəyyən edən fiziki və ya hüquqi şəxsdir. Bu Məxfilik Siyasəti məqsədilə, biz sizin məlumatlarınızın Məlumat Nəzarətçisiyik.</p>
+    <p><strong>MƏLUMAT EMALÇILARI (VƏ YA XİDMƏT TƏCHİZATÇILARI):</strong> məlumatları Məlumat Nəzarətçisi adından işləyən hər hansı fiziki və ya hüquqi şəxsdir. Məlumatlarınızı daha effektiv şəkildə emal etmək üçün müxtəlif Xidmət Təchizatçılarının xidmətlərindən istifadə edə bilərik.</p>
+    <p><strong>MƏLUMAT SUBYEKTI:</strong> Şəxsi Məlumatların mövzusunu təşkil edən hər hansı canlı fərddir.</p>
+    <p><strong>İSTİFADƏÇİ:</strong> bizim Xidmətimizdən istifadə edən fərdi şəxsdir. İstifadəçi Məlumat Subyektinə uyğun gəlir və Şəxsi Məlumatların mövzusunu təşkil edir.</p>
+
+    <h2>Məlumatların Toplanması və İstifadəsi</h2>
+    <p>Biz, Xidmətimizi təqdim etmək və təkmilləşdirmək məqsədilə müxtəlif növ məlumatları toplayırıq.</p>
+
+    <h2>Toplanan Məlumat Növləri</h2>
+    <h3>Şəxsi Məlumatlar</h3>
+    <p>Xidmətimizdən istifadə edərkən, sizinlə əlaqə yaratmaq və sizi müəyyən etmək üçün istifadə edilə bilən bəzi şəxsi məlumatlarınızı təqdim etməyinizi istəyə bilərik (“Şəxsi Məlumatlar”). Şəxsi Məlumatlara aşağıdakılar daxildir:</p>
+    <ul>
+        <li>Email ünvanı</li>
+        <li>Ad və soyad</li>
+        <li>Telefon nömrəsi</li>
+        <li>Ünvan, Ölkə, Ştat, Rayon, Poçt indeksi, Şəhər</li>
+        <li>Cookie-lər və İstifadə Məlumatları</li>
+    </ul>
+    <p>Biz Şəxsi Məlumatlarınızı sizə xəbər bülletenləri, marketinq və ya promosyon materialları və maraq doğura biləcək digər məlumatları göndərmək üçün istifadə edə bilərik. Bu cür əlaqələrdən imtina etmək üçün “unsubscribe” keçidinə keçə bilərsiniz.</p>
+
+ 
+        </div>
+
+    </div>
+</div>
+
+<style>
+        .privacy-main{
+        width: 100%;
+        height: auto;
+        background-color: rgb(248,248,248);
+        display: flex;
+        justify-content: center;
+    }
+    .privacy-box {
+        margin-top: 24px;
+        margin-bottom: 24px;
+        width: 1100px;
+        border-radius: 10px;
+        height: auto;
+        background-color: white;
+        display: flex;
+        justify-content: space-between;
+        padding: 40px 25px;
+        h1, h2 {
+            color: #333;
+        }
+        p {
+            margin: 10px 0;
+        }
+
+    }
+
+
+</style>
 @endsection
