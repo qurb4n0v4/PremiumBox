d8a8b59f8fc9eb84ca6f8b502cc713a5e406585c
hagigataghazada <agazade.heqiqet@gmail.com>
2024-11-28 16:24:13 +0400

    indicators added

--- resources/views/front/home/imagesSlider.blade.php
+++ resources/views/front/home/imagesSlider.blade.php
@@ -10,7 +10,7 @@
                 <div class="carousel-caption d-none d-md-block text-end position-absolute top-50 end-10 p-1">
                     <h5>Ön sifariş qəbul olunur,</h5>
                     <h4>Yeni İl Hədiyyəsi</h4>
-                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Yeni İlin tam zamanı, yeni kolleksiya burada</p>
                     <button class="new-year-gifts">Kolleksiyaya Bax</button>
                 </div>
             </div>
@@ -19,9 +19,9 @@
                 <img src="{{ asset('assets/front/img/BDNJHXxUQPv11llTquieK9dhOYBTmXRDTD37SDbA.webp') }}"
                      class="d-block w-100" alt="Gift box">
                 <div class="carousel-caption d-none d-md-block text-end position-absolute top-50 end-10 p-1">
-                    <h5>Ön sifariş qəbul olunur,</h5>
-                    <h4>Yeni İl Hədiyyəsi</h4>
-                    <p>Lorem ipsum dolor sit amet.</p>
+                    <h5>Şərablı Qutu</h5>
+                    <h4>Şərab Səbətləri</h4>
+                    <p>Sevdikləriniz üçün özəl şərab qutuları</p>
                     <button class="new-year-gifts" style="background-color: transparent">Kolleksiyaya Bax</button>
                 </div>
             </div>
@@ -29,12 +29,13 @@
             <div class="carousel-item">
                 <img src="{{ asset('assets/front/img/quj77SXomFlKtMAov5gQhy731kMC89cUcpq56JwK.webp') }}"
                      class="d-block w-100" alt="Gift box">
+
                 <div class="carousel-caption d-none d-md-block text-end position-absolute top-50 p-1"
                      style="right: 5%; left: auto;">
-                    <h5>Ön sifariş qəbul olunur,</h5>
-                    <h4>Yeni İl Hədiyyəsi</h4>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <button class="new-year-gifts" style="background-color: transparent">Kolleksiyaya Bax</button>
+                    <h5>Hədiyyələrinizi Özəlləşdirin</h5>
+                    <h4>Eyni Gündə Adresə Təslim</h4>
+                    <p>Qutunuzu, hədiyyələrinizi seçin və özəl sözlərinizi yazın.</p>
+                    <button class="new-year-gifts" style="background-color: transparent">Bir Qutu Yarat</button>
                 </div>
             </div>
             {{--            Slide 4--}}
@@ -43,10 +44,11 @@
                      class="d-block w-100" alt="Gift box">
                 <div class="carousel-caption d-none d-md-block text-end position-absolute top-50 p-1"
                      style="right: 5%; left: auto;">
-                    <h5>Ön sifariş qəbul olunur,</h5>
-                    <h4>Yeni İl Hədiyyəsi</h4>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <button class="new-year-gifts" style="background-color: transparent">Kolleksiyaya Bax</button>
+                    <h5>GiftBox-a Xoşgəlmişsiniz!</h5>
+                    <h4>İlk Sifarişə Özəl <br>
+                        Ödənişsiz Karqo Xidməti</h4>
+                    <p>Promosyon Kodu: BFCIE79FHGWU</p>
+                    <button class="new-year-gifts" style="background-color: transparent">Hədiyyənizi Özəlləşdirin</button>
                 </div>
             </div>
             {{--            Slide 5--}}
@@ -55,10 +57,10 @@
                      class="d-block w-100" alt="Gift box">
                 <div class="carousel-caption d-none d-md-block text-end position-absolute top-50 p-1"
                      style="right: 5%; left: auto;">
-                    <h5>Ön sifariş qəbul olunur,</h5>
-                    <h4>Yeni İl Hədiyyəsi</h4>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <button class="new-year-gifts" style="background-color: transparent">Kolleksiyaya Bax</button>
+                    <h5>Hədiyyənizi Özəlləşdirin</h5>
+                    <h4>Yazı tipinizi seçin</h4>
+                    <p>Qutunuzu seçin və özəlləşdirin</p>
+                    <button class="new-year-gifts" style="background-color: transparent">Bir Qutu Yarat</button>
                 </div>
             </div>
             {{--            Slide 6--}}
@@ -67,10 +69,10 @@
                      class="d-block w-100" alt="Gift box">
                 <div class="carousel-caption d-none d-md-block text-end position-absolute top-50 p-1"
                      style="right: 5%; left: auto;">
-                    <h5>Ön sifariş qəbul olunur,</h5>
-                    <h4>Yeni İl Hədiyyəsi</h4>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <button class="new-year-gifts" style="background-color: transparent">Kolleksiyaya Bax</button>
+                    <h5>Özünüz Yaradın</h5>
+                    <h4>Özəl Hədiyyə Qutusu</h4>
+                    <p>Qutunuzu, hədiyyələrinizi seçin və özəl sözlərinizi yazın.</p>
+                    <button class="new-year-gifts" style="background-color: transparent">Buraya Daxil Olun</button>
                 </div>
             </div>
             {{--            Slide 7--}}
@@ -79,10 +81,10 @@
                      class="d-block w-100" alt="Gift box">
                 <div class="carousel-caption d-none d-md-block text-end position-absolute top-50 p-1"
                      style="right: 5%; left: auto;">
-                    <h5>Ön sifariş qəbul olunur,</h5>
-                    <h4>Yeni İl Hədiyyəsi</h4>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <button class="new-year-gifts" style="background-color: transparent">Kolleksiyaya Bax</button>
+                    <h5>İlhama Ehtiyacınız Var?</h5>
+                    <h4>Hazır Qutu</h4>
+                    <p>Daha sürətli, problemsiz, özəl hədiyyə qutuları</p>
+                    <button class="new-year-gifts" style="background-color: transparent">Hazır Qutu Al</button>
                 </div>
             </div>
             {{--            Slide 8 --}}
@@ -91,10 +93,9 @@
                      class="d-block w-100" alt="Gift box">
                 <div class="carousel-caption d-none d-md-block text-end position-absolute top-50 p-1"
                      style="right: 5%; left: auto;">
-                    <h5>Ön sifariş qəbul olunur,</h5>
-                    <h4>Yeni İl Hədiyyəsi</h4>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <button class="new-year-gifts" style="background-color: transparent">Kolleksiyaya Bax</button>
+                    <h5>Daha Böyük Hədiyyələrə Ehtiyacınız Var?</h5>
+                    <h4>Korporativ Və Özəl Hədiyyələr</h4>
+                    <button class="new-year-gifts" style="background-color: transparent">Daha Çox...</button>
                 </div>
             </div>
             {{--            Slide 9--}}
@@ -103,20 +104,25 @@
                      class="d-block w-100" alt="Gift box">
                 <div class="carousel-caption d-none d-md-block text-end position-absolute top-50 p-1"
                      style="right: 5%; left: auto;">
-                    <h5>Ön sifariş qəbul olunur,</h5>
-                    <h4>Yeni İl Hədiyyəsi</h4>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <button class="new-year-gifts" style="background-color: transparent">Kolleksiyaya Bax</button>
+                    <h5>Yenidən Yarat</h5>
+                    <h4>Heyrətamiz Hekayələr</h4>
+                    <p>Çünki Biz inanırıq ki, hədiyyələr
+                        bənzərsiz hekayələrinə görə <br> hər bir insan üçün fərqli mənalar daşıyır. </p>
+                    <button class="new-year-gifts" style="background-color: transparent">GiftBox Haqqqında</button>
                 </div>
             </div>
         </div>
         <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselGiftSlides" data-bs-slide-to="0" class="active"
                     aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselGiftSlides" data-bs-slide-to="1"
-                    aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselGiftSlides" data-bs-slide-to="2"
-                    aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselGiftSlides" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselGiftSlides" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselGiftSlides" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#carouselGiftSlides" data-bs-slide-to="4" aria-label="Slide 5"></button>
+            <button type="button" data-bs-target="#carouselGiftSlides" data-bs-slide-to="5" aria-label="Slide 6"></button>
+            <button type="button" data-bs-target="#carouselGiftSlides" data-bs-slide-to="6" aria-label="Slide 7"></button>
+            <button type="button" data-bs-target="#carouselGiftSlides" data-bs-slide-to="7" aria-label="Slide 8"></button>
+            <button type="button" data-bs-target="#carouselGiftSlides" data-bs-slide-to="8" aria-label="Slide 9"></button>
         </div>
     </div>
 @endsection
