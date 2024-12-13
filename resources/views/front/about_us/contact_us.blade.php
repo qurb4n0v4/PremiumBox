c163b732ed2ce2a15b319f6638965ccbeaabc072
GulnurMirzayeva <gmirzyeva1@gmail.ru>
2024-12-13 16:01:15 +0400

    Conflict between buttons

--- resources/views/front/about_us/contact_us.blade.php
+++ resources/views/front/about_us/contact_us.blade.php
@@ -35,4 +35,144 @@
         </div>
     </div>
 </div>
+
+<style>
+
+    /*Contact-us starts*/
+    .contact-main{
+        width: 100%;
+        height: auto;
+        background-color: rgb(248,248,248);
+        display: flex;
+        justify-content: center;
+    }
+    .contact-box {
+        margin-top: 24px;
+        width: 1100px;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+        height: auto;
+        background-color: white;
+        display: flex;
+        justify-content: space-between;
+        padding: 40px 25px;
+
+    }
+    .contact-left{
+        width: 49%;
+        height: auto;
+    }
+    .contact-right{
+        width: 49%;
+        height: auto;
+        display: flex;
+        flex-direction: column;
+    }
+
+
+    .contact-right p{
+        color: #a3907a;
+        font-size: 0.9rem;
+    }
+
+    .contact-left img {
+        width: 507px;
+        height: 676px;
+        margin-top: 29px;
+    }
+    .contact-right h1{
+        font-size: 2.25rem;
+        color: #a3907a;
+    }
+    .contact-left p{
+        font-size: 18px;
+        margin-top: 17px;
+        color: #a3907a;
+    }
+
+    form {
+        border-radius: 5px;
+        width: 100%;
+    }
+
+    label {
+        display: block;
+        margin: 10px 0 5px;
+    }
+    input, textarea, button {
+        width: 100%;
+        padding: 10px;
+        margin-bottom: 10px;
+        border: 1px solid #ccc;
+        border-radius: 3px;
+    }
+    button {
+        background-color: #4CAF50;
+        color: white;
+        border: none;
+        cursor: pointer;
+    }
+    button:hover {
+        background-color: #45a049;
+    }
+
+
+    @media screen and (max-width: 1200px) {
+        .contact-left img {
+            width: 417px;
+            height: 556px;
+        }
+
+    }
+
+
+    @media screen and (max-width: 1100px) {
+        .contact-box{
+            width: 960px;
+        }
+
+
+    }
+    @media screen and (max-width: 991px) {
+        .contact-box{
+            width: 720px;
+        }
+        .contact-left img {
+            width: 297px;
+            height: 396px;
+        }
+
+    }
+
+    @media screen and (max-width: 768px) {
+        .contact-box {
+            width: 540px;
+            display: flex;
+            flex-direction: column;
+            gap: 50px;
+        }
+
+        .contact-left {
+            width: 100%;
+        }
+        .contact-left img{
+            width: 444px;
+            height: 592px;
+        }
+        .contact-right{
+            width: 100%;
+        }
+    }
+
+    @media screen and (max-width: 574px) {
+        .contact-box{
+            width: 100%;
+        }
+        .contact-left img{
+            width: 100%;
+            height: auto;
+        }
+    }
+    /*Contact-us ends*/
+</style>
 @endsection
