8182c69755b83a317d26cbbc539c3a5b529160a7
hagigataghazada <agazade.heqiqet@gmail.com>
2025-01-07 12:21:14 +0400

    settings changed

--- resources/views/front/user/orders.blade.php
+++ resources/views/front/user/orders.blade.php
@@ -1,10 +1,31 @@
 @extends('front.user.profile')
 
 @section('profile-content')
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">My Orders</h5>
-            <p>You have no orders yet!</p>
+    <div class="orders-card card">
+        <div class="orders-card-body">
+            <div class="orders-header">
+                <h5 class="orders-title">Sifarişlərim</h5>
+            </div>
         </div>
     </div>
 @endsection
+
+<style>
+    .orders-card {
+        background-color: #fff;
+        border: none !important;
+    }
+
+    .orders-title {
+        font-size: 20px;
+        /*font-weight: bold;*/
+        color: #a3907a !important;
+        padding-bottom: 10px;
+        margin-bottom: 20px;
+    }
+
+    .orders-header {
+        display: flex;
+        /*align-items: center;*/
+    }
+</style>
